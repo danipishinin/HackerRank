834f42bc22156524459323dbd103e4f95cfa351a
danipishinin <danielepishinin@gmail.com>
2022-08-21 21:38:40 -0300

    functions cpp

---
+++ c++/c-tutorial-functions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d){
+    int maior = a;
+    if(b > maior)
+        maior = b;
+    if(c > maior)
+        maior = c;
+    if(d > maior)
+        maior = d;
+    return maior;
+}
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
